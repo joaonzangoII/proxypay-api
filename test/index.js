0efdccdb2de662839451db8f27d532bc6104acc7
Firmino Changani <flowck96@gmail.com>
2017-01-01 00:19:12 +0100

    Initial commit of 2️⃣0️⃣1️⃣7️⃣

---
+++ test/index.js
@@ -0,0 +1,25 @@
+// Enable strict mode
+"use strict";
+
+// Import dependencies
+const mocha = require("mocha");
+const expect = require("chai").expect;
+const ProxyPay = require("../lib/proxypay");
+
+// Start with the magic here
+describe("References", function(){
+
+	describe("Get all References", function(){
+
+	});
+
+	describe("Get a specific Reference", function(){
+
+	});
+
+	describe("Generate References", function(){
+		it("Generate a new Reference", function(){
+			
+		});
+	});
+});
